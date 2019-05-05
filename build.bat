19cb32834691a1ffca89facf701b9bee06bfe009
Adam Haynes <haynesy.adam@gmail.com>
2019-05-05 17:16:27 +1000

    Initial commit

---
+++ build.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -DHANDMADE_WIN32=1 -FC -Z7 
+set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib
+
+REM TODO - can we just build both with one exe?
+
+IF NOT EXIST bin mkdir bin
+pushd bin
+
+REM 32-bit build
+REM cl %CommonCompilerFlags% ..\handmade\code\win32_handmade.cpp /link -subsystem:windows,5.1 %CommonLinkerFlags%
+
+REM 64-bit build
+del *.pdb > NUL 2> NUL
+REM cl %CommonCompilerFlags% -Fmhandmade.map -Fdhandmade.pdb ../code/hot_load_code.cpp -LD /link -incremental:no -opt:ref -PDB:handmade_%date:~-4,4%%date:~-10,2%%date:~-7,2%_%time:~3,2%%time:~6,2%.pdb -EXPORT:GameGetSoundSamples -EXPORT:GameUpdateAndRender
+cl %CommonCompilerFlags% -Fmwin32_handmade.map ../code/main.cpp /link %CommonLinkerFlags%
+popd
