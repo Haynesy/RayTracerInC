42f74d258f974b1fa8424677b998a2bb935a840c
Adam Haynes <haynesy.adam@gmail.com>
2019-05-18 11:26:29 +1000

    Will allow an image .ppm file to be generated with an RGB blend pattern

--- code/main.cpp
+++ code/main.cpp
@@ -1,3 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void writeFile(int width, int height){
+    cout << "P3\n" << width << " " << height << "\n255\n";    
+    for(int y = height - 1; y >= 0; y--){
+        for(int x = 0; x < width; x++){
+            float r  = float(x) / float(width);
+            float g  = float(y) / float(height);
+            float b = 0.2f;
+
+            int red = int(255.99 * r);
+            int green = int(255.99 * g);
+            int blue = int(255.99 * b);
+
+            cout << red << " " << green << " " << blue << "\n";
+        }
+    }
+}
+
 int main(int argc, char *argv[]){
+
+    writeFile(200, 100);
     return 0;
 }
---
+++ run.bat
@@ -0,0 +1,6 @@
+
+REM Create image and output to file
+.\bin\main.exe > .\bin\image.ppm
+
+REM View image
+.\bin\image.ppm
